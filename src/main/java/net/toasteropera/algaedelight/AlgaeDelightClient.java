b29c4c26bdaab5fd9b20ed13b8657f073abd5a7d
ToasterOpera <toasteropera11@gmail.com>
2026-01-25 15:00:50 -0800

    Initial Setup

--- src/main/java/net/toasteropera/algaedelight/AlgaeDelightClient.java
+++ src/main/java/net/toasteropera/algaedelight/AlgaeDelightClient.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.toasteropera.algaedelight;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
@@ -11,13 +11,14 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = ExampleMod.MODID, dist = Dist.CLIENT)
+@Mod(value = AlgaeDelight.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = ExampleMod.MODID, value = Dist.CLIENT)
-public class ExampleModClient {
-    public ExampleModClient(ModContainer container) {
+@EventBusSubscriber(modid = AlgaeDelight.MODID, value = Dist.CLIENT)
+public class AlgaeDelightClient {
+    public AlgaeDelightClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
-        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // The config screen is accessed by going to the Mods screen > clicking
+        // on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
@@ -25,7 +26,7 @@ public class ExampleModClient {
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
-        ExampleMod.LOGGER.info("HELLO FROM CLIENT SETUP");
-        ExampleMod.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        AlgaeDelight.LOGGER.info("HELLO FROM CLIENT SETUP");
+        AlgaeDelight.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
